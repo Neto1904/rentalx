5dc37f48f30bd34bcfce69e01ce0abf5cf1d03f2
Neto1904 <netohsa66@gmail.com>
2021-10-12 14:04:48 -0300

    Add initial file

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import express from 'express'
+import 'express-async-errors'
 import swagger from 'swagger-ui-express'
 
-import { routes } from './routes'
-import swaggerFile from './swagger.json'
+import './database'
+import swaggerFile from '../docs/hidro.json'
+import './shared/container'
+import { checkError } from './errors/checkError'
+import { routes } from './routes/index.routes'
 
 const app = express()
 
 app.use(express.json())
 app.use('/api-docs', swagger.serve, swagger.setup(swaggerFile))
 app.use(routes)
+app.use(checkError)
 
 app.listen(3333, () => {
   console.log('Server is running')
