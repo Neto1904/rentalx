bd3dcd1af9bfc6dd1a03c3d31f52cbc6fe54924c
Neto1904 <netohsa66@gmail.com>
2021-10-12 14:05:19 -0300

    Add custom types for express

---
+++ src/@types/express/index.d.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-unused-vars
+declare namespace Express {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  export interface Request {
+    id: string
+  }
+}
