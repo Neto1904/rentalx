cb9cc81a536f4412c12486fcfdcece7691905f15
Neto1904 <netohsa66@gmail.com>
2022-01-04 19:06:35 -0300

    Add user authentication use case

--- src/modules/accounts/useCases/authenticateUser/AunthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AunthenticateUserUseCase.ts
@@ -2,8 +2,11 @@ import { compare } from 'bcrypt'
 import { sign } from 'jsonwebtoken'
 import { inject, injectable } from 'tsyringe'
 
-import { AppError } from '@errors/AppError'
+import authConfig from '@config/auth'
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository'
+import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository'
+import { IDateProvider } from '@shared/container/providers/dateProvider/IDateProvider'
+import { AppError } from '@shared/errors/AppError'
 
 interface IRequest {
   email: string
@@ -16,13 +19,18 @@ interface IResponse {
     email: string
   }
   token: string
+  refreshToken: string
 }
 
 @injectable()
 class AuthenticateUserUseCase {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+    @inject('UsersTokensRepository')
+    private usersTokensRepository: IUsersTokensRepository,
+    @inject('DateProvider')
+    private dateProvider: IDateProvider
   ) {}
   async execute({ email, password }: IRequest): Promise<IResponse> {
     const user = await this.usersRepository.findByEmail(email)
@@ -33,12 +41,31 @@ class AuthenticateUserUseCase {
     if (!passwordMatch) {
       throw new AppError('Email or password incorrect')
     }
-    const token = sign({}, '56e3c0f4e3224b87f78ad23edbd13736', {
+    const token = sign({}, authConfig.secretToken, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn: authConfig.expiresIn,
     })
 
-    return { user: { name: user.name, email: user.email }, token }
+    const refreshToken = sign(
+      { email: user.email },
+      authConfig.secretRefreshToken,
+      {
+        subject: user.id,
+        expiresIn: authConfig.expiresInRefreshToken,
+      }
+    )
+
+    const expiresDate = await this.dateProvider.addDays(
+      authConfig.refreshTokenExpirationDays
+    )
+
+    await this.usersTokensRepository.create({
+      userId: user.id,
+      refreshToken,
+      expiresDate,
+    })
+
+    return { user: { name: user.name, email: user.email }, token, refreshToken }
   }
 }
 
