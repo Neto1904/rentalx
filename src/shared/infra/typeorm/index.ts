73249981e0e08aae7d59277abf981de57922d141
Neto1904 <netohsa66@gmail.com>
2022-01-07 16:48:43 -0300

    Change database config

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -8,8 +8,6 @@ interface IOptions {
 
 getConnectionOptions().then((options) => {
   const newOptions = options as IOptions
-  newOptions.host =
-    process.env.NODE_ENV === 'test' ? 'localhost' : 'database_ignite' // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
   newOptions.port = process.env.NODE_ENV === 'test' ? 5433 : 5432
   newOptions.database = process.env.NODE_ENV === 'test' ? 'rentx-test' : 'rentx'
   createConnection({
